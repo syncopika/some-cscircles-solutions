49842d1f775033233d77cddea9328e49de5342f8
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-11-24 20:48:46 -0500

    Update auto-decryption.py

--- chap15/auto-decryption.py
+++ chap15/auto-decryption.py
@@ -1,3 +1,6 @@
+letterGoodness = [0.0817, 0.0149, 0.0278, 0.0425, 0.127, 0.0223, 0.0202, 0.0609, 0.0697, 0.0015, 0.0077, 0.0402, 0.0241, 0.0675, 0.0751, 0.0193, 0.0009, 0.0599, 0.0633, 0.0906, 0.0276, 0.0098, 0.0236, 0.0015, 0.0197, 0.0007]
+#each number corresponds to a letter in the alphabet in sequence, i.e. A, B, C,...
+
 def decrypt(thing):
    a = thing
    alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
