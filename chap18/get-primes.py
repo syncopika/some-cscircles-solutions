493121fba9e96f2ffb9a58f425ece3970e4f5c75
Nick H. (熊志文) <syncopika@users.noreply.github.com>
2015-11-29 10:12:27 -0500

    Create get-primes.py

---
+++ chap18/get-primes.py
@@ -0,0 +1,25 @@
+def getNonPrimes(n):
+   primes = []
+   for i in range(2,n+1):
+      for j in range(2,i):
+         if i%j == 0:
+            primes.append(i)
+            break
+   return primes
+
+
+def getPrimes(n):
+   a = getNonPrimes(n)
+   b = []
+   c = []
+   #fill array with all numbers to n
+   for i in range(2,n+1):
+      b.append(i)
+   #use resulting array from getNonPrimes to eliminate non-primes in b
+   for i in range(0,len(b)):
+      if b[i] not in a:
+         c.append(b[i])
+   
+   return c
+   
+   #example: print(getPrimes(10)) => output: [2,3,5,7]
