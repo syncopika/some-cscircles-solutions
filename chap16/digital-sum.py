5030b5e4a6721e6ed2ac78f02ec9e796663643bd
Nicholas H <syncopika@users.noreply.github.com>
2018-10-10 20:39:41 -0400

    Update digital-sum.py

--- chap16/digital-sum.py
+++ chap16/digital-sum.py
@@ -2,3 +2,9 @@ def digitalSum(n):
   if n == 0: 
       return 0
   return (n % 10 + digitalSum(int(n / 10))) 
+    
+def digitalSum(n):
+   if n < 10:
+      return n 
+   else:
+      return digitalSum(n//10) + digitalSum(n%10)
